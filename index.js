e110a49ef877e0899e7d1bf4c48f01bf4a0e89c2
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-02-05 12:33:01 +0200

    3_4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -21,6 +21,11 @@ const persons = [
         "name": "Lea Kutvonen",
         "number": "040-123456",
         "id": 4
+    },
+    {
+        "name": "kettu",
+        "number": "123",
+        "id": 5
     }
 ]
 
@@ -41,6 +46,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
