0aa45127082618b4b0c8a582f2195c7be3013d61
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-02-11 17:11:41 +0200

    fuu

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://<user>:<password>@ds125388.mlab.com:25388/fs3'
+const url = 'mongodb://timo:tietokanta@ds125388.mlab.com:25388/fs3'
 
 mongoose.connect(url)
 //mongoose.Promise = global.Promise
